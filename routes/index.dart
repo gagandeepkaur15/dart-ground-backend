5aae1f77faa8c94fd68a345398d4686ab91666e2
Gagandeep-lucify <gagandeepkaur1523@gmail.com>
2024-10-27 12:55:50 +0530

    Dart-ground backend

---
+++ routes/index.dart
@@ -0,0 +1,91 @@
+import 'dart:io';
+import 'dart:isolate';
+import 'package:dart_frog/dart_frog.dart';
+
+Future<Response> onRequest(RequestContext context) async {
+  if (context.request.method != HttpMethod.post) {
+    return Response(statusCode: HttpStatus.methodNotAllowed);
+  }
+  final body = await context.request.body();
+  final code = body;
+
+  print("Received code: $code");
+
+  final result = await _runCodeInIsolate(code);
+
+  print("Result from isolate: $result");
+
+  return Response.json(body: {'result': result});
+}
+
+Future<Map<String, dynamic>> _runCodeInIsolate(String code) async {
+  final receivePort = ReceivePort();
+
+  await Isolate.spawn(_isolateRunner, [receivePort.sendPort, code]);
+
+  final result = await receivePort.first as Map<String, dynamic>;
+
+  return result;
+}
+
+Future<void> _isolateRunner(List<dynamic> args) async {
+  final sendPort = args[0] as SendPort;
+  final code = args[1] as String;
+
+  print("Isolate running code: $code");
+
+  try {
+    final tempFile = await _createTempDartFile(code);
+    final result = await _runDartCode(tempFile);
+    await tempFile.delete();
+
+    print("Execution result: $result");
+
+    sendPort.send(result);
+  } catch (e, stacktrace) {
+    print('Error in isolate execution: $e');
+    print('Stacktrace: $stacktrace');
+    sendPort.send({
+      'output': 'Error: $e',
+      'executionTime': null,
+      'memoryUsed': null,
+    });
+  }
+}
+
+Future<File> _createTempDartFile(String code) async {
+  final tempDir = Directory.systemTemp;
+  final tempFile = File('${tempDir.path}/temp_dart_code.dart');
+  await tempFile.writeAsString(code);
+
+  return tempFile;
+}
+
+Future<Map<String, dynamic>> _runDartCode(File dartFile) async {
+  final stopwatch = Stopwatch()..start();
+  final initialMemory = ProcessInfo.currentRss;
+
+  final result = await Process.run('dart', [dartFile.path]);
+
+  final executionTime = stopwatch.elapsedMilliseconds;
+  final finalMemory = ProcessInfo.currentRss;
+  final memoryUsed = finalMemory - initialMemory;
+
+  print("Process result: ${result.stdout}");
+  print("Process error: ${result.stderr}");
+  print("Process exit code: ${result.exitCode}");
+
+  if (result.exitCode != 0) {
+    return {
+      'output': result.stderr.toString(),
+      'executionTime': executionTime.toString(),
+      'memoryUsed': null,
+    };
+  } else {
+    return {
+      'output': result.stdout.toString(),
+      'executionTime': '${executionTime}ms',
+      'memoryUsed': '${(memoryUsed / 1024).toStringAsFixed(2)}KB',
+    };
+  }
+}
